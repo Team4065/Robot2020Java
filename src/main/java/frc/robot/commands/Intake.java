b2fc7f5b3c007ed18c86b11b0c83395e65ff05cd
Berethekid <43358442+Berethekid@users.noreply.github.com>
2021-02-23 15:28:19 -0500

    Enter

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -5,11 +5,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
 
 public class Intake extends CommandBase {
+   Intake m_intake;
   /** Creates a new Intake. */
-  public Intake() {
+  public Intake(Intake intake) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_intake = intake;
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
@@ -18,7 +22,9 @@ public class Intake extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
